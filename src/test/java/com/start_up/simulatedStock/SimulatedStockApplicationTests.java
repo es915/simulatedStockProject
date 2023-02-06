64d8f570295ae6f3e39148937d980c57833be823
eulseon <eulseon2004@gmail.com>
2023-02-06 10:01:41 +0900

    first commit

---
+++ src/test/java/com/start_up/simulatedStock/SimulatedStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start_up.simulatedStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatedStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
